3af4e7bc7e8a2064e52bf0e52a49231fdb7083ac
Manuel Diaz <manuel.diaz@here.com>
2023-06-23 22:14:16 -0600

    project finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import UserInput from "./components/UserInput/UserInput";
+import ResultsTable from "./components/ResultsTable/ResultsTable";
+import { useState } from "react";
 
 function App() {
+  const [userInput,setUserInput]=useState(null)
+  
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  const yearlyData = []; // per-year results
+
+  if(userInput){
+
+      let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
+      const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+      const expectedReturn = +userInput['expected-return'] / 100;
+      const duration = +userInput['duration'];
+
+      for (let i = 0; i < duration; i++) {
+        const yearlyInterest = currentSavings * expectedReturn;
+        currentSavings += yearlyInterest + yearlyContribution;
+        yearlyData.push({
+          year: i + 1,
+          yearlyInterest: yearlyInterest,
+          savingsEndOfYear: currentSavings,
+          yearlyContribution: yearlyContribution,
+        });
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <UserInput onCalculate={calculateHandler}/>  
+
+      {!userInput && <p style={{textAlign:'center'}}>No Investments yet.</p>}
+      {userInput && <ResultsTable data={yearlyData} initialInvestment={userInput['current-savings']}/>}
     </div>
   );
 }
